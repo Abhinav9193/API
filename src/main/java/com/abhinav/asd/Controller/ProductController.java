f34664dad07fd34f646ba8be019242a58d6c69fd
Abhinav TIwari <abhinavtiwari021@gmail.com>
2025-08-31 23:52:59 +0530

    API

--- src/main/java/com/abhinav/asd/Controller/ProductController.java
+++ src/main/java/com/abhinav/asd/Controller/ProductController.java
@@ -4,14 +4,11 @@ import java.util.*;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-
     private List<String> products = new ArrayList<>(Arrays.asList("Laptop", "Phone", "Tablet"));
-
     @GetMapping
     public List<String> getAllProducts() {
         return products;
     }
-
     @PostMapping
     public String addProduct(@RequestBody String product) {
         products.add(product);
